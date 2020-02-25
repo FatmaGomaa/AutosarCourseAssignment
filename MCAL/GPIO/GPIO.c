071220c2059937f570855cb438f0243381aa4c33
Mohanad-Sallam <mohanad20295@gmail.com>
2020-02-25 17:31:35 +0400

    Creating GPIO Files

---
+++ MCAL/GPIO/GPIO.c
@@ -0,0 +1,20 @@
+/********************************
+*Author: Mostafa Ghareeb        *
+*        Mohanad Fawzy Sallam   * 
+*Version: v01                   *
+*Date: 25/02/2020               *
+*********************************/
+#include "STD_Types.h"
+#include "GPIO.h"
+error_status GPIO_Init (u8 Port, u8 Pin, u8 Mode)
+{
+  
+}
+error_status GPIO_WritePin (u8 Port, u8 Pin, u8 Value)
+{
+  
+}
+error_status GPIO_ReadPin (u8 Port, u8 Pin, u8 * Value)
+{
+  
+}
---
+++ MCAL/GPIO/GPIO.h
@@ -0,0 +1,33 @@
+/********************************
+*Author: Mostafa Ghareeb        *
+*        Mohanad Fawzy Sallam   * 
+*Version: v01                   *
+*Date: 25/02/2020               *
+*********************************/
+/* Ports Numbers*/
+#define PORT_A 1
+#define PORT_B 2
+#define PORT_C 3
+#define PORT_D 4
+#define PORT_E 5
+#define PORT_F 6
+/* Pins Numbers*/
+#define PIN_0 0
+#define PIN_1 1
+#define PIN_2 2
+#define PIN_3 3
+#define PIN_4 4
+#define PIN_5 5
+#define PIN_6 6
+#define PIN_7 7
+/* GPIO operation Modes*/
+ #define IP_PULL_UP 1
+ 
+ 
+
+/*Dscription: This API shall initalize GPIO Configuration Mode*/
+error_status GPIO_Init (u8 Port, u8 Pin, u8 Mode);
+/*Dscription: This API shall Write GPIO pin output value*/
+error_status GPIO_WritePin (u8 Port, u8 Pin, u8 Value);
+/*Dscription: This API shall read GPIO pin input value*/*/
+error_status GPIO_ReadPin (u8 Port, u8 Pin, u8 * Value);
