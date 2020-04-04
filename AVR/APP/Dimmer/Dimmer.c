43c267a7e7e97042a42576f30851ce07da783294
Mohanad-Sallam <mohanad20295@gmail.com>
2020-04-04 20:36:32 +0400

    update dimmer.c
    if w elseif mistake :D

--- AVR/APP/Dimmer/Dimmer.c
+++ AVR/APP/Dimmer/Dimmer.c
@@ -24,7 +24,7 @@ error_status Dimmer_UpdateStatus(void)
 	local_error  = RTE_ReadDoorStatus(&DoorStatus);
 	if (DOOR_OPEN == DoorStatus)
 	{local_error |= RTE_WriteLightStatus(LIGHT_ON);}
-	else if (DOOR_OPEN == DoorStatus)
+	else if (DOOR_CLOSED == DoorStatus)
 	{local_error |= RTE_WriteLightStatus(LIGHT_OFF);}
 	else
 	{local_error = E_NOK;}
